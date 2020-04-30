fff692fac77e8286b433e3ad19b1f1e7a3aa2502
elchd <59011427+elchd@users.noreply.github.com>
2020-04-30 13:03:29 +0200

    Update load_dico.py

--- TexasHoldem/clustering/load_dico.py
+++ TexasHoldem/clustering/load_dico.py
@@ -9,3 +9,29 @@ t.stop()
 
 print(t.total_run_time())
 print(preflop)
+
+##################
+
+from common import constantes as c
+import pandas as pd
+import numpy as np
+import pprint
+
+cartes =[]
+cartes[:0] = c.VALEURS
+matrice = np.zeros([len(cartes), len(cartes)])
+
+for key in preflop.keys():
+    val1 = key[2]
+    col1 = key[3]
+    val2 = key[8]
+    col2 = key[9]
+
+    if col1 == col2:
+        matrice[c.VALEURS.find(val1)][c.VALEURS.find(val2)] = preflop[key]  #suited
+    else:
+        matrice[c.VALEURS.find(val2)][c.VALEURS.find(val1)] = preflop[key]  #unsuited
+     
+df = pd.DataFrame(matrice, columns = cartes)
+df['cartes'] = cartes
+df.set_index(['cartes'], inplace=True)
